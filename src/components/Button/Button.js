8623f6a646a9dd3d64ab6e9164a2ee8442a7c3f7
Omer <omrebres@gmail.com>
2021-03-23 19:36:15 +0200

    Added text to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Text from "components/Text";
 import * as S from "./style";
 
 const Button = ({ label, onClick }) => {
     return (
         <S.BaseButton>
-            <S.Button onClick={onClick}>{label}</S.Button>
+            <S.Button onClick={onClick}>
+                <Text color='red' size='72px'>
+                    {label}
+                </Text>
+            </S.Button>
         </S.BaseButton>
     );
 };
