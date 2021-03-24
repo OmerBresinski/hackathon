ebf54c32183f01c3f4772bc32399c20266c0d3fe
shiranZe <szelnir@gmail.com>
2021-03-24 20:08:24 +0200

    users api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home, Feed, Project, CreateProject } from "pages";
 import NavBar from "components/NavBar";
 import { URL } from "./constant";
+import { getUsers } from './service'
+
+
 
 const App = () => {
+
+    useEffect(() => {
+        fetchFakeData()
+    }, [])
+
+    const fetchFakeData = async () => {
+        const users = await getUsers()
+        const projects = [{ title: 'a', description: 'sadaas', id: 123, userProfileImage: '' }, { title: 'fdsdwe', description: 'sadfnejfeufheuaas', id: 123 }]
+
+    }
+
     return (
         <>
             <NavBar />
