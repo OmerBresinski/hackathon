b0d352079241b8957e9b21294beda9bb63d39f44
Omer Bresinski <omer@powerlink.co.il>
2021-03-25 15:42:18 +0200

    New submit

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,7 +1,5 @@
 import React from "react";
 import * as S from "./style";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 const TextInput = ({ fieldName, value, onChange, placeholder, onClick }) => {
     const handleChange = (e) => {
@@ -11,7 +9,7 @@ const TextInput = ({ fieldName, value, onChange, placeholder, onClick }) => {
     return (
         <S.BaseTextInput>
             <S.Input onChange={handleChange} value={value} placeholder={placeholder} />
-            <FontAwesomeIcon icon={faPaperPlane} onClick={onClick} />
+            <div onClick={onClick} style={{ height: "20px", width: "20px", cursor: "pointer", background: "lightgreen", borderRadius: "3px" }} />
         </S.BaseTextInput>
     );
 };
